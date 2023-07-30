2b74371d49cbdca292c374ca8b7755d60327f211
codewithgoodluck <goodluckmordi4@gmail.com>
2023-07-30 21:57:55 +0100

    done

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ require('express-async-errors');
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const fileUpload = require('express-fileupload');
+
+
+// USE V2
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
 
 // database
 const connectDB = require('./db/connect');
@@ -13,16 +23,23 @@ const productRouter = require('./routes/productRoutes')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.use(express.static('./public'));
+app.use(express.json());
+app.use(fileUpload({ useTempFiles: true }))
+
 app.get('/', (req, res) => {
   res.send('<h1>File Upload Starter</h1>');
 });
+
+app.use('/api/v1/products', productRouter)
+
 
 // middleware
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 //
-app.use('/api/v1/products', productRouter)
+
 
 const port = process.env.PORT || 3000;
 
--- controllers/productController.js
+++ controllers/productController.js
@@ -2,11 +2,13 @@ const Product = require('../models/Product')
 const {StatusCodes} = require("http-status-codes")
 
 const createProduct = async(req, res) =>{
-    res.send('create product');
+    const product = await Product.create(req.body);
+    res.status(StatusCodes.CREATED).json({ product });
 }
 
 const getAllProducts = async(req, res) =>{
-    res.send('List of products')
+    const products = await Product.find({});
+    res.status(StatusCodes.OK).json({ products });
 }
 
 module.exports = {
